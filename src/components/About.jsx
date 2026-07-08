abb541e857e2674b4e8afba757e6b66eda207f47
Anirudh Koppuravuri <221583116+Anirudh-os@users.noreply.github.com>
2026-07-08 19:15:11 +0530

    Update profile image in code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import stackImage from '../assets/about/Anirudh_Koppuravuri.png';
+import stackImage from '../assets/about/HeroImage.png.png';
 import { aboutContent } from '../data/portfolioData';
 
 // Tech stack SVG icons rendered inline for crisp rendering
